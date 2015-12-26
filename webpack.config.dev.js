72aeecbf82a437d96186a863aca4e1f00c7d4abd
voxlol <voxlol@outlook.com>
2015-12-26 07:40:17 -0600

    Get a working install of SASS-bootstrap

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,6 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
+    'bootstrap-loader',
     'webpack-hot-middleware/client',  // Lets HMR know to re-render
     './src/index'
   ],
@@ -29,6 +30,10 @@ module.exports = {
       test: /\.scss$/,
       loaders: ['style', 'css?sourceMap', 'sass?sourceMap'],
       include: path.join(__dirname, 'src')
-    }]
+    },
+    { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
+    { test: /\.(woff2?|svg)$/, loader: 'url?limit=10000' },
+    { test: /\.(ttf|eot)$/, loader: 'file' },
+    ]
   }
 };
