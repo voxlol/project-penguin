74a837dacffb16f7d9aa8577e9293b7ce7fc39a9
voxlol <voxlol@outlook.com>
2015-12-26 13:44:34 -0600

    Fix webpack config bug with babel-loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -24,6 +24,7 @@ module.exports = {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel-loader'],
+      exclude: path.join(__dirname, 'node_modules')
     },
     {
       test: /\.scss$/,
