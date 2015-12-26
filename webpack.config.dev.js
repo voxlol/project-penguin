d3205713d73d0af9a0c5e9265b70b0967fa42b97
voxlol <voxlol@outlook.com>
2015-12-26 06:47:23 -0600

    Add file,url loaders and setup Bootstrap to work in webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -24,7 +24,14 @@ module.exports = {
       test: /\.js$/,
       loaders: ['babel-loader'],
       include: path.join(__dirname, 'src')
-    },{
+    },
+    { test: /\.css$/,loader: 'style-loader!css-loader' },
+    { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+    { test: /\.(woff|woff2)$/, loader:'url?prefix=font/&limit=5000' },
+    { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+    { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'
+    },
+    {
       test: /\.scss$/,
       loaders: ['style', 'css?sourceMap', 'sass?sourceMap'],
       include: path.join(__dirname, 'src')
